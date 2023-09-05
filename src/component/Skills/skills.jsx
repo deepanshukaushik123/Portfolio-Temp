773136c6dcd91d0b91d93ad5c791e659845686ba
Deepanshu Kaushik <deepanshukaushik@Deepanshus-MacBook-Pro.local>
2023-09-05 13:06:55 +0530

    changes

--- src/component/Skills/skills.jsx
+++ src/component/Skills/skills.jsx
@@ -2,12 +2,39 @@ import React from 'react'
 import './skills.css'
 import WebDesign from "../../assets/website-design.png";
 import AppDesign from "../../assets/app-design.png";
+import UIDesign from "../../assets/ui-design.png";
+// import AppDesign from "../../assets/app-design.png";
+// import AppDesign from "../../assets/app-design.png";
 
 const Skills = () => {
   return (
    <section id="skills">
    <span className="skillTitle">What I do</span>
    <span className="skillDesc">I am a skilled and passionate Web Developer with experience in</span>
+   <div className="skillBars">
+    <div className="skillBar">
+        <img src={UIDesign} alt="" className="skillBarImg" />
+        <div className="skillBarText">
+            <h2></h2>
+            <p></p>
+        </div>
+    </div>
+    <div className="skillBar">
+        <img src="" alt="" className="skillBarImg" />
+        <div className="skillBarText">
+            <h2></h2>
+            <p></p>
+        </div>
+    </div>
+    <div className="skillBar">
+        <img src="" alt="" className="skillBarImg" />
+        <div className="skillBarText">
+            <h2></h2>
+            <p></p>
+        </div>
+    </div>
+   </div>
+   
    </section>
   )
 }
